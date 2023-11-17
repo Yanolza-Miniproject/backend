76e10a109f3a9cc8e4fb1876bd5668e4b7f3f82a
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2023-11-17 19:24:54 +0900

    [test] test

--- src/main/java/com/miniproject/MiniProjectApplication.java
+++ src/main/java/com/miniproject/MiniProjectApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MiniProjectApplication {
-
     public static void main(String[] args) {
+        System.out.println("test");
         SpringApplication.run(MiniProjectApplication.class, args);
     }
 
