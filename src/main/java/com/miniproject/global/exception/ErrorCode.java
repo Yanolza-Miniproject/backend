d2ded900a84d1f291e2379f1aabf51368ab5af9c
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2023-11-20 18:30:10 +0900

    [init] global 기본세팅

---
+++ src/main/java/com/miniproject/global/exception/ErrorCode.java
@@ -0,0 +1,31 @@
+package com.miniproject.global.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum ErrorCode {
+    //사용자
+    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 회원입니다."),
+    //숙소
+
+    //객실
+    ROOM_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 객실입니다.");
+    //장바구니 상품
+
+    //장바구니
+
+    //주문
+
+    //결제
+
+    //좋아요
+
+    private HttpStatus httpStatus;
+    private String message;
+
+    ErrorCode(HttpStatus httpStatus, String message) {
+        this.httpStatus = httpStatus;
+        this.message = message;
+    }
+}
---
+++ src/main/java/com/miniproject/global/util/ResponseDTO.java
@@ -0,0 +1,32 @@
+package com.miniproject.global.util;
+
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class ResponseDTO<T> {
+
+    private final String message;
+    private final T data;
+
+    @Builder
+    private ResponseDTO(String message, T data) {
+        this.message = message;
+        this.data = data;
+    }
+
+    public static ResponseDTO<Object> res(final String message) {
+        return ResponseDTO.<Object>builder()
+
+            .message(message)
+            .build();
+    }
+
+    public static <T> ResponseDTO<T> res(final String message, final T data) {
+        return ResponseDTO.<T>builder()
+            .message(message)
+            .data(data)
+            .build();
+    }
+}
