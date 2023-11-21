f72474eb2ad20a1103f87789d66694083ad63e26
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2023-11-21 13:06:19 +0900

    [init] Like 초기 설정

---
+++ src/main/java/com/miniproject/domain/Like/repository/LikeRepository.java
@@ -0,0 +1,9 @@
+package com.miniproject.domain.Like.repository;
+
+
+import com.miniproject.domain.Like.entity.Like;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LikeRepository extends JpaRepository<Like,Long> {
+
+}
---
+++ src/main/java/com/miniproject/domain/Like/service/LikeService.java
@@ -0,0 +1,8 @@
+package com.miniproject.domain.Like.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LikeService {
+
+}
