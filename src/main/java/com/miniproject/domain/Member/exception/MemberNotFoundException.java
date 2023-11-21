81be671cbc101e1ed80f532ba1218d56e2b293a9
happymink <minkyun@kakao.com>
2023-11-21 13:02:30 +0900

    [feat] MemberNotFoundException 추가

---
+++ src/main/java/com/miniproject/domain/Member/exception/MemberNotFoundException.java
@@ -0,0 +1,10 @@
+package com.miniproject.domain.Member.exception;
+
+import com.miniproject.global.exception.ApplicationException;
+import com.miniproject.global.exception.ErrorCode;
+
+public class MemberNotFoundException extends ApplicationException {
+    private static final ErrorCode errorCode = ErrorCode.MEMBER_NOT_FOUND;
+
+    public MemberNotFoundException() {super(errorCode);}
+}
