b51b89a82383599bc2144a7587b2e871cae974f2
JuHwan Kwon <kjh307ok@gmail.com>
2023-11-22 17:40:27 +0900

    [feat] BasketController 장바구니 조회 로직 추가

--- src/main/java/com/miniproject/domain/basket/controller/BasketController.java
+++ src/main/java/com/miniproject/domain/basket/controller/BasketController.java
@@ -1,8 +1,33 @@
 package com.miniproject.domain.basket.controller;
 
+import com.miniproject.domain.basket.dto.response.BasketResponseDto;
+import com.miniproject.domain.basket.service.BasketService;
+import com.miniproject.global.util.ResponseDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/baskets")
 public class BasketController {
+
+    @Autowired
+    private BasketService basketService;
+
+    @GetMapping
+    public ResponseEntity<ResponseDTO<BasketResponseDto>> getBasket(){
+        return ResponseEntity.ok().body(ResponseDTO
+                .res("장바구니 조회 성공", basketService.getBasket()));
+    }
+
+//    @PostMapping("/orders")
+//    public ResponseEntity<ResponseDTO> orderBasket(@RequestBody CheckBasketRequestDto dto) {
+//
+//    }
+
 
 }
