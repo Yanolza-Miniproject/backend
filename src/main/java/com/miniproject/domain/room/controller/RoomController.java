ffe4365e5c6ed696f1a0778c6e541c8efc082ac2
JuHwan Kwon <kjh307ok@gmail.com>
2023-11-22 17:44:10 +0900

    [feat] RoomController 장바구니에 추가, 구매 로직 추가

--- src/main/java/com/miniproject/domain/room/controller/RoomController.java
+++ src/main/java/com/miniproject/domain/room/controller/RoomController.java
@@ -1,8 +1,35 @@
 package com.miniproject.domain.room.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.miniproject.domain.basket.entity.Basket;
+import com.miniproject.domain.basket.service.BasketService;
+import com.miniproject.domain.room.dto.request.RoomRegisterRequestDto;
+import com.miniproject.domain.room.service.RoomService;
+import com.miniproject.global.util.ResponseDTO;
+import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/v1/rooms")
 public class RoomController {
+    @Autowired
+    private RoomService roomService;
 
+    @PostMapping("/{room_id}/baskets")
+    public ResponseEntity<ResponseDTO> registerRoom(@PathVariable Long roomId
+            , @RequestBody RoomRegisterRequestDto dto) {
+        roomService.creatRoomInBasket(roomId,dto);
+//        member 구현 시 , 변경되는 메소드
+//        roomService.creatRoomInBasket(roomId,dto,member);
+        return ResponseEntity.ok().body(ResponseDTO.res("장바구니에 객실이 담겼습니다."));
+    }
+    @PostMapping("/{room_id}/orders")
+    public ResponseEntity<ResponseDTO> orderRoom(@PathVariable Long roomId
+            , @RequestBody RoomRegisterRequestDto dto){
+        roomService.createSinglePurchase(roomId,dto);
+//        member 구현 시 , 변경되는 메소드
+//        roomService.createSinglePurchase(roomId,dto,member);
+        return ResponseEntity.ok().body(ResponseDTO.res("객실 구매가 진행됩니다."));
+    }
 }
