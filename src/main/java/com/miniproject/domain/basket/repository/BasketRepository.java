afff717b0dd86b212dc62909cad84f492ad4fba1
JuHwan Kwon <kjh307ok@gmail.com>
2023-11-22 17:41:02 +0900

    [feat] Status로 장바구니 서치하는 로직 추가

--- src/main/java/com/miniproject/domain/basket/repository/BasketRepository.java
+++ src/main/java/com/miniproject/domain/basket/repository/BasketRepository.java
@@ -1,8 +1,13 @@
 package com.miniproject.domain.basket.repository;
 
 import com.miniproject.domain.basket.entity.Basket;
+import com.miniproject.domain.basket.entity.BasketStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BasketRepository extends JpaRepository<Basket,Long> {
+
+    List<Basket> findByBasketStatus(BasketStatus basketStatus);
 
 }
