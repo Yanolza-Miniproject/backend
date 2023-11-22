7cdfdedbd0ea2dfc9cd85908d916f5d4649774a9
JuHwan Kwon <kjh307ok@gmail.com>
2023-11-22 17:42:47 +0900

    [feat] Room roomImage 연관관계 추가

--- src/main/java/com/miniproject/domain/room/entity/Room.java
+++ src/main/java/com/miniproject/domain/room/entity/Room.java
@@ -1,13 +1,11 @@
 package com.miniproject.domain.room.entity;
 
 import com.miniproject.domain.accommodation.entity.Accommodation;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
@@ -22,6 +20,9 @@ public class Room {
     @ManyToOne
     @JoinColumn(name = "accommodation_id")
     private Accommodation accommodation;
+
+    @OneToMany(mappedBy = "room")
+    private List<RoomImage> roomImages = new ArrayList<>();
 
     private String name;
 
