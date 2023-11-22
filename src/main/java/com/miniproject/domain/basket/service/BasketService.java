27d420fc3290a65c167641c869fb4239fe4d8713
JuHwan Kwon <kjh307ok@gmail.com>
2023-11-22 17:41:45 +0900

    [feat] 장바구니 조회, 활성화된 장바구니 가져오기, 단일 구매용 장바구니 서비스 로직 구현

--- src/main/java/com/miniproject/domain/basket/service/BasketService.java
+++ src/main/java/com/miniproject/domain/basket/service/BasketService.java
@@ -1,8 +1,43 @@
 package com.miniproject.domain.basket.service;
 
+import com.miniproject.domain.basket.dto.request.CheckBasketRequestDto;
+import com.miniproject.domain.basket.dto.response.BasketResponseDto;
+import com.miniproject.domain.basket.entity.Basket;
+import com.miniproject.domain.basket.entity.BasketStatus;
+import com.miniproject.domain.basket.exception.BasketDuplicateActivateException;
+import com.miniproject.domain.basket.repository.BasketRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class BasketService {
+    @Autowired
+    private BasketRepository basketRepository;
+
+    public BasketResponseDto getBasket(){
+        return new BasketResponseDto(getActivateBasket());
+    }
+    public Basket getActivateBasket(){
+        List<Basket> nowBasket = basketRepository.findByBasketStatus(BasketStatus.ACTIVATE);
+        Basket basket = switch (nowBasket.size()) {
+            case 1 -> nowBasket.get(0);
+            case 0 -> new Basket();
+            default -> throw new BasketDuplicateActivateException();
+        };
+        return basket;
+    }
+    public Basket getSinglePurchase(){
+        Basket basket = new Basket();
+        basket.ChangeStatus(BasketStatus.SINGLE_PURCHASE);
+        return basket;
+    }
+
+    public void registerOrder(Basket basket, CheckBasketRequestDto dto) {
+
+    }
 
 }
