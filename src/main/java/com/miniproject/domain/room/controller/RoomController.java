61378c3cc1b40b9ad75ba07a0f635f3917d42665
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2023-11-23 18:34:11 +0900

    [feat] RoomController 장바구니 담기, 주문하기 로직 구현

--- src/main/java/com/miniproject/domain/room/controller/RoomController.java
+++ src/main/java/com/miniproject/domain/room/controller/RoomController.java
@@ -1,35 +1,40 @@
 package com.miniproject.domain.room.controller;
 
-import com.miniproject.domain.basket.entity.Basket;
-import com.miniproject.domain.basket.service.BasketService;
+import com.miniproject.domain.member.entity.Member;
 import com.miniproject.domain.room.dto.request.RoomRegisterRequestDto;
 import com.miniproject.domain.room.service.RoomService;
 import com.miniproject.global.util.ResponseDTO;
-import lombok.extern.java.Log;
-import org.springframework.beans.factory.annotation.Autowired;
+import java.net.URI;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/rooms")
+@RequiredArgsConstructor
 public class RoomController {
-    @Autowired
-    private RoomService roomService;
+
+
+    private final RoomService roomService;
 
     @PostMapping("/{room_id}/baskets")
-    public ResponseEntity<ResponseDTO> registerRoom(@PathVariable Long roomId
-            , @RequestBody RoomRegisterRequestDto dto) {
-        roomService.creatRoomInBasket(roomId,dto);
-//        member 구현 시 , 변경되는 메소드
-//        roomService.creatRoomInBasket(roomId,dto,member);
+    public ResponseEntity<ResponseDTO> registerRoom(@PathVariable Long room_id
+        , @RequestBody RoomRegisterRequestDto dto, Member member) {
+        roomService.creatRoomInBasket(room_id, dto, member);
+
         return ResponseEntity.ok().body(ResponseDTO.res("장바구니에 객실이 담겼습니다."));
     }
+
     @PostMapping("/{room_id}/orders")
-    public ResponseEntity<ResponseDTO> orderRoom(@PathVariable Long roomId
-            , @RequestBody RoomRegisterRequestDto dto){
-        roomService.createSinglePurchase(roomId,dto);
-//        member 구현 시 , 변경되는 메소드
-//        roomService.createSinglePurchase(roomId,dto,member);
-        return ResponseEntity.ok().body(ResponseDTO.res("객실 구매가 진행됩니다."));
+    public ResponseEntity<ResponseDTO> orderRoom(@PathVariable Long room_id
+        , @RequestBody RoomRegisterRequestDto dto, Member member) {
+
+        Long orderId = roomService.createSingleOrders(room_id, dto, member);
+        return ResponseEntity.created(URI.create("api/v1/orders/" + orderId))
+            .body(ResponseDTO.res("객실 구매가 진행됩니다.", orderId));
     }
 }
