e4bcacf72f2e2a3dbf0b6c307fc7e746835a0bcc
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2023-11-23 18:34:25 +0900

    [feat] RoomService 장바구니 담기, 주문하기 로직 구현

--- src/main/java/com/miniproject/domain/room/service/RoomService.java
+++ src/main/java/com/miniproject/domain/room/service/RoomService.java
@@ -1,58 +1,71 @@
 package com.miniproject.domain.room.service;
 
 import com.miniproject.domain.basket.entity.Basket;
-import com.miniproject.domain.basket.entity.BasketStatus;
-import com.miniproject.domain.basket.repository.BasketRepository;
+import com.miniproject.domain.basket.exception.BasketEmptyException;
 import com.miniproject.domain.basket.service.BasketService;
 import com.miniproject.domain.member.entity.Member;
+import com.miniproject.domain.orders.entity.Orders;
+import com.miniproject.domain.orders.repository.OrdersRepository;
 import com.miniproject.domain.room.dto.request.RoomRegisterRequestDto;
 import com.miniproject.domain.room.entity.Room;
 import com.miniproject.domain.room.entity.RoomInBasket;
+import com.miniproject.domain.room.entity.RoomInOrders;
+import com.miniproject.domain.room.exception.RoomInBasketNotFoundException;
 import com.miniproject.domain.room.exception.RoomNotFoundException;
 import com.miniproject.domain.room.repository.RoomInBasketRepository;
+import com.miniproject.domain.room.repository.RoomInOrdersRepository;
 import com.miniproject.domain.room.repository.RoomRepository;
+import java.time.LocalDateTime;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
+@RequiredArgsConstructor
 public class RoomService {
-    @Autowired
-    private RoomRepository roomRepository;
-    @Autowired
-    private BasketService basketService;
-    @Autowired
-    private RoomInBasketRepository roomInBasketRepository;
 
-    public void creatRoomInBasket(Long roomId, RoomRegisterRequestDto dto){
+    private final RoomRepository roomRepository;
+    private final BasketService basketService;
+    private final RoomInBasketRepository roomInBasketRepository;
+    private final OrdersRepository ordersRepository;
+    private final RoomInOrdersRepository roomInOrdersRepository;
+
+    public RoomInBasket creatRoomInBasket(Long roomId, RoomRegisterRequestDto dto, Member member){
         Room room = roomRepository.findById(roomId).orElseThrow(
                 RoomNotFoundException::new);
-        Basket activateBasket = basketService.getActivateBasket();
+        Basket activateBasket = basketService.getActivateBasket(member);
         RoomInBasket roomInBasket = RoomInBasket.builder()
                 .checkInAt(dto.getCheckInAt())
                 .checkOutAt(dto.getCheckOutAt())
                 .numberOfGuests(dto.getNumberOfGuests())
                 .room(room)
-//                .member(member)
+                .member(member)
                 .basket(activateBasket)
                 .build();
-        roomInBasketRepository.save(roomInBasket);
         activateBasket.RegisterRoom(roomInBasket);
-
+        return roomInBasketRepository.save(roomInBasket);
     }
 
-    public void createSinglePurchase(Long roomId, RoomRegisterRequestDto dto){
+    public Long createSingleOrders(Long roomId, RoomRegisterRequestDto dto,Member member) {
         Room room = roomRepository.findById(roomId).orElseThrow(
-                RoomNotFoundException::new);
-        Basket singlePurchaseBasket = basketService.getSinglePurchase();
-        RoomInBasket roomInBasket = RoomInBasket.builder()
-                .checkInAt(dto.getCheckInAt())
-                .checkOutAt(dto.getCheckOutAt())
-                .numberOfGuests(dto.getNumberOfGuests())
-                .room(room)
-//                .member(member)
-                .basket(singlePurchaseBasket)
-                .build();
-        singlePurchaseBasket.RegisterRoom(roomInBasket);
+            RoomNotFoundException::new);
+        Orders orders = Orders.builder()
+            .orderAt(LocalDateTime.now())
+            .member(member)
+            .totalPrice(room.getPrice())
+            .totalCount(dto.getNumberOfGuests()).build();
+        Orders save = ordersRepository.save(orders);
+        RoomInOrders roomInOrders = RoomInOrders.builder()
+            .checkInAt(dto.getCheckInAt())
+            .checkOutAt(dto.getCheckOutAt())
+            .numberOfGuests(dto.getNumberOfGuests())
+            .room(room)
+            .member(member)
+            .orders(save)
+            .build();
+        roomInOrdersRepository.save(roomInOrders);
+        return save.getId();
     }
-
 }
