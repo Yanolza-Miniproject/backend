bf187fce3c2fc3bdf56ea78a93904c2765093eb2
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2023-11-23 18:37:19 +0900

    [feat] 결제 공통 ResponseDto 구현

---
+++ src/main/java/com/miniproject/domain/payment/dto/response/PaymentResponseDto.java
@@ -0,0 +1,34 @@
+package com.miniproject.domain.payment.dto.response;
+
+
+import com.miniproject.domain.orders.entity.Orders;
+import com.miniproject.domain.payment.entity.Payment;
+import com.miniproject.domain.room.dto.response.RoomInOrdersGetResponseDto;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PaymentResponseDto {
+    private Long id;
+    private int totalPrice;
+    private int totalCount;
+    private LocalDateTime paymentAt;
+    private String paymentType;
+    private List<RoomInOrdersGetResponseDto> rooms;
+
+    public PaymentResponseDto (Payment payment) {
+        this.id = payment.getId();
+        this.totalPrice = payment.getOrders().getTotalPrice();
+        this.totalCount = payment.getOrders().getTotalCount();
+        this.paymentAt = payment.getPaymentAt();
+        this.paymentType = payment.getPaymentType();
+        this.rooms = payment.getOrders().getRoomInOrders().stream().map(
+                roomInOrders -> new RoomInOrdersGetResponseDto(roomInOrders))
+            .collect(Collectors.toList());
+    }
+}
