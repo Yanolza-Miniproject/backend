693f4af84ba0b0085bd31e02866cb98825e32c3d
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2023-11-23 18:38:39 +0900

    [feat] 결제 상태 구현

---
+++ src/main/java/com/miniproject/domain/room/repository/RoomInOrdersRepository.java
@@ -0,0 +1,8 @@
+package com.miniproject.domain.room.repository;
+
+import com.miniproject.domain.room.entity.RoomInOrders;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomInOrdersRepository extends JpaRepository<RoomInOrders, Long> {
+
+}
