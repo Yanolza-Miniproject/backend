9aba65005efffe351786db069043245fa046cacb
kimdongmin <meena2003@naver.com>
2023-11-23 17:32:05 +0900

    [feat] 좋아요 등록, 취소, 조회 기능 1차 구현

--- src/main/java/com/miniproject/domain/wish/controller/WishController.java
+++ src/main/java/com/miniproject/domain/wish/controller/WishController.java
@@ -1,11 +1,17 @@
 package com.miniproject.domain.wish.controller;
 
+import com.miniproject.domain.member.entity.Member;
+import com.miniproject.domain.wish.dto.WishResponses;
+import com.miniproject.domain.wish.entity.Wish;
 import com.miniproject.domain.wish.service.WishService;
+import com.miniproject.global.util.ResponseDTO;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static com.miniproject.domain.wish.dto.WishResponses.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,21 +21,24 @@ public class WishController {
     private final WishService wishService;
 
     // 좋아요 누르기
-    @PostMapping
-    void like() {
-
+    @PostMapping("/{accommodation_id}")
+    public ResponseDTO addWish(@PathVariable(name = "accommodation_id") Long accommodationId,
+                              @AuthenticationPrincipal Member member) {
+        wishService.saveWish(accommodationId, member);
+        return ResponseDTO.res("좋아요 성공");
     }
 
     // 좋아요 취소
-    @PostMapping
-    void disLike() {
-
+    @DeleteMapping("/{accommodation_id}")
+    public ResponseDTO cancelWish(@PathVariable(name = "accommodation_id") Long accommodationId,
+                        @AuthenticationPrincipal Member member) {
+        wishService.deleteWish(accommodationId, member);
+        return ResponseDTO.res("좋아요 취소");
     }
 
     // 회원의 좋아요 리스트 조회
     @GetMapping
-    void getList() {
-
+    public ResponseDTO<List<AccommodationWishResDto>> getWishes(@AuthenticationPrincipal Member member) {
+        return ResponseDTO.res("좋아요 리스트 조회 성공", wishService.getWishes(member));
     }
-
 }
--- src/main/java/com/miniproject/domain/wish/entity/Wish.java
+++ src/main/java/com/miniproject/domain/wish/entity/Wish.java
@@ -4,8 +4,11 @@ import com.miniproject.domain.accommodation.entity.Accommodation;
 import com.miniproject.domain.member.entity.Member;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.FetchType.*;
 
 @Entity
 @Getter
@@ -17,11 +20,19 @@ public class Wish {
     @Column(name = "wish_id")
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    // 숙소가 가진 좋아요 수를 확인하기 위해서 양방향 해야 하나?
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "accommodation_id")
     private Accommodation accommodation;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    // 멤버가 가진 숙소를 확인하기 위해 양방향 해야 하나?
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
+
+    @Builder
+    protected Wish(Accommodation accommodation, Member member) {
+        this.accommodation = accommodation;
+        this.member = member;
+    }
 }
