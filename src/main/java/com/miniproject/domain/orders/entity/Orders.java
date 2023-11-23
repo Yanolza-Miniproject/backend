a09f2672850bd3290daee3ec73051233e5c7140b
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2023-11-23 18:31:57 +0900

    [feat] Orders builder 패턴 추가

--- src/main/java/com/miniproject/domain/orders/entity/Orders.java
+++ src/main/java/com/miniproject/domain/orders/entity/Orders.java
@@ -2,15 +2,21 @@ package com.miniproject.domain.orders.entity;
 
 import com.miniproject.domain.basket.entity.Basket;
 import com.miniproject.domain.member.entity.Member;
+import com.miniproject.domain.room.entity.RoomInBasket;
+import com.miniproject.domain.room.entity.RoomInOrders;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -25,11 +31,25 @@ public class Orders {
 
     private LocalDateTime orderAt;
 
+    private int totalPrice;
+    private int totalCount;
+
     @ManyToOne
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToOne
-    @JoinColumn(name = "basket_id")
-    private Basket basket;
+
+    @OneToMany(mappedBy = "orders")
+    private List<RoomInOrders> roomInOrders  = new ArrayList<>();
+
+    @Builder
+    public Orders(LocalDateTime orderAt, int totalPrice, int totalCount,
+        Member member,List<RoomInOrders> roomInOrders) {
+        this.orderAt = orderAt;
+        this.totalPrice = totalPrice;
+        this.totalCount = totalCount;
+        this.member = member;
+        this.roomInOrders = roomInOrders;
+        this.orderAt = LocalDateTime.now();
+    }
 }
