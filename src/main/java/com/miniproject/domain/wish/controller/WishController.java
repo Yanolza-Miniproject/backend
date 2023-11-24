f12d70177262572dc5d6dda69461df32461d1329
junmo95 <junmop950@gmail.com>
2023-11-24 19:15:30 +0900

    Feat: 좋아요 부분 수정 accommodation 기능 부분과 연결하는데 발생한 문제 수정

--- src/main/java/com/miniproject/domain/wish/controller/WishController.java
+++ src/main/java/com/miniproject/domain/wish/controller/WishController.java
@@ -31,14 +31,14 @@ public class WishController {
     // 좋아요 취소
     @DeleteMapping("/{accommodation_id}")
     public ResponseDTO cancelWish(@PathVariable(name = "accommodation_id") Long accommodationId,
-                        @AuthenticationPrincipal Member member) {
+                         Member member) {
         wishService.deleteWish(accommodationId, member);
         return ResponseDTO.res("좋아요 취소");
     }
 
     // 회원의 좋아요 리스트 조회
     @GetMapping
-    public ResponseDTO<List<AccommodationWishResDto>> getWishes(@AuthenticationPrincipal Member member) {
+    public ResponseDTO<List<AccommodationWishResDto>> getWishes( Member member) {
         return ResponseDTO.res("좋아요 리스트 조회 성공", wishService.getWishes(member));
     }
 }
