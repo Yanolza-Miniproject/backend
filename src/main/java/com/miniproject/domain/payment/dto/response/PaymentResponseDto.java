d9b5990abfcae64161f66c9a55de0b7d9cd082eb
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2023-11-27 23:25:54 +0900

    [feat] 빌더 패턴 추가

--- src/main/java/com/miniproject/domain/payment/dto/response/PaymentResponseDto.java
+++ src/main/java/com/miniproject/domain/payment/dto/response/PaymentResponseDto.java
@@ -8,11 +8,15 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
 public class PaymentResponseDto {
     private Long id;
     private int totalPrice;
--- src/main/java/com/miniproject/domain/room/entity/RoomInBasket.java
+++ src/main/java/com/miniproject/domain/room/entity/RoomInBasket.java
@@ -8,13 +8,13 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-;
 
 @Entity
 @Getter
@@ -35,7 +35,6 @@ public class RoomInBasket {
     @JoinColumn(name = "basket_id")
     private Basket basket;
 
-
     @ManyToOne
     @JoinColumn(name = "member_id")
     private Member member;
@@ -44,10 +43,12 @@ public class RoomInBasket {
     @JoinColumn(name = "room_id")
     private Room room;
 
-    @Builder
-    public RoomInBasket(LocalDateTime checkInAt, LocalDateTime checkOutAt,
-        int numberOfGuests, Basket basket, Member member, Room room) {
 
+
+    @Builder
+    public RoomInBasket(Long id, LocalDateTime checkInAt, LocalDateTime checkOutAt,
+        int numberOfGuests, Basket basket, Member member, Room room) {
+        this.id = id;
         this.checkInAt = checkInAt;
         this.checkOutAt = checkOutAt;
         this.numberOfGuests = numberOfGuests;
@@ -55,6 +56,5 @@ public class RoomInBasket {
         this.basket = basket;
         this.member = member;
     }
-
 
 }
