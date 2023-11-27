d6556fc71025dce6d9e0bfe77b5bf6babc24def8
kimdongmin <meena2003@naver.com>
2023-11-27 18:50:34 +0900

    [test] Wish Repository 테스트 코드 작성

--- src/test/java/com/miniproject/wish/repository/WishRepositoryTest.java
+++ src/test/java/com/miniproject/wish/repository/WishRepositoryTest.java
@@ -1,28 +1,108 @@
 package com.miniproject.wish.repository;
 
+import com.miniproject.domain.accommodation.entity.Accommodation;
 import com.miniproject.domain.accommodation.repository.AccommodationRepository;
+import com.miniproject.domain.member.entity.Member;
 import com.miniproject.domain.member.repository.MemberRepository;
+import com.miniproject.domain.wish.entity.Wish;
 import com.miniproject.domain.wish.repository.WishRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @DataJpaTest
 class WishRepositoryTest {
 
     @Autowired
     private WishRepository wishRepository;
+    private Member member;
+    private Accommodation accommodation;
 
-    @Autowired
-    private AccommodationRepository accommodationRepository;
+    @BeforeEach
+    void init() {
+        wishRepository.deleteAll();
 
-    @Autowired
-    private MemberRepository memberRepository;
+        member = Member.builder()
+                .email("test@test.com")
+                .name("tester")
+                .password("123")
+                .password("010-1234-5678").build();
 
+        accommodation = Accommodation.builder()
+                .name("신라호텔")
+                .type("관광호텔")
+                .address("경기도 용인시 수지구")
+                .phoneNumber("02-1234-1234")
+                .homepage("www.test.com")
+                .infoDetail("테스트용 호텔입니다.")
+                .thumbnailUrl(null)
+                .categoryParking(true)
+                .categoryCooking(true)
+                .categoryPickup(false)
+                .categoryAmenities(false)
+                .categoryDiningArea(true)
+                .checkIn(LocalDateTime.parse("2020-01-01T11:00:00"))
+                .checkOut(LocalDateTime.parse("2020-01-01T11:00:00"))
+                .wishCount(0)
+                .viewCount(0).build();
+    }
 
+    @Test
+    @DisplayName("좋아요 저장 리포지토리 테스트")
+    void saveWishTest() {
+        // given
+        Wish wish = Wish.builder()
+                .accommodation(accommodation)
+                .member(member)
+                .build();
 
+        // when
+        Wish saved = wishRepository.save(wish);
 
+        // then
+        assertThat(saved).extracting("accmmodation", "member").containsExactly(
+                wish.getAccommodation(), wish.getMember());
+    }
 
+    @Test
+    @DisplayName("좋아요 삭제 리포지토리 테스트")
+    void deleteWish() {
+        // given
+        Wish wish = Wish.builder()
+                .accommodation(accommodation)
+                .member(member)
+                .build();
+        Wish saved = wishRepository.save(wish);
 
+        // when
+        wishRepository.delete(wish);
+
+        // then
+        boolean isPresent = wishRepository.existsById(saved.getId());
+        assertThat(isPresent).isFalse();
+    }
+
+    @Test
+    @DisplayName("사용자와 숙박 정보로 좋아요 조회 테스트")
+    void findMyMemberANdAccommodation() {
+        // given
+        Wish wish = Wish.builder()
+                .accommodation(accommodation)
+                .member(member)
+                .build();
+        Wish saved = wishRepository.save(wish);
+
+        // when
+        Wish fined = wishRepository.findByMemberAndAccommodation(member, accommodation).get();
+
+        // then
+        assertThat(saved.getAccommodation()).isEqualTo(fined.getAccommodation());
+        assertThat(saved.getMember()).isEqualTo(fined.getMember());
+    }
 }
