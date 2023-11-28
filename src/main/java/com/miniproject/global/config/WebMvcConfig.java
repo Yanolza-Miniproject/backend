c0631bcbcc9b624dc99e0b28b4457f2a0c178464
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2023-11-28 19:16:32 +0900

    [feat] allowedOrigin 추가

--- src/main/java/com/miniproject/global/config/WebMvcConfig.java
+++ src/main/java/com/miniproject/global/config/WebMvcConfig.java
@@ -3,6 +3,7 @@ package com.miniproject.global.config;
 import com.miniproject.global.resolver.LoginInfoArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -18,5 +19,16 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         argumentResolvers.add(loginInfoArgumentResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOriginPatterns("http://localhost:8080/**","http://localhost:5173/**",
+                "https://yaja-project.web.app/**")
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+            .allowCredentials(true)
+            .exposedHeaders("*");
+
     }
 }
