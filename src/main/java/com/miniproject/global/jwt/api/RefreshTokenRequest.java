83306c0a53861c6caef7364fe352538961000a82
happymink <minkyun@kakao.com>
2023-11-29 19:59:05 +0900

    [fix] RefreshToken 요청 스펙 변경
    motivation:
    - Refresh Token을 통해 AccessToken을 재발급할 때, AccessToken을 받지 않습니다.

--- src/main/java/com/miniproject/global/jwt/api/RefreshTokenRequest.java
+++ src/main/java/com/miniproject/global/jwt/api/RefreshTokenRequest.java
@@ -3,7 +3,6 @@ package com.miniproject.global.jwt.api;
 import jakarta.validation.constraints.NotBlank;
 
 public record RefreshTokenRequest(
-        @NotBlank String accessToken,
         @NotBlank String refreshToken
 ) {
 }
